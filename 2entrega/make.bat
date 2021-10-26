57029a04354b684f1d1211ac4b972302f847b527
maikgiordano98 <70980802+maikgiordano98@users.noreply.github.com>
2021-10-26 19:42:49 -0300

    compilacion en windows

--- 2entrega/make.bat
+++ 2entrega/make.bat
@@ -1,4 +1,4 @@
 C:\GnuWin32\bin\flex.exe Lexico.l
 C:\GnuWin32\bin\bison.exe -dyv Sintactico.y
-C:\MinGW\bin\gcc.exe -o parser.exe -w lex.yy.c y.tab.c symbol_table.c
+C:\MinGW\bin\gcc.exe -std=c99 -o parser.exe -w lex.yy.c y.tab.c symbol_table.c tercetos.c stack.c
 
